3909b547e356fc301dfae742b649122887dbb5e4
ssplot56 <ssplot56@gmail.com>
2023-02-03 18:53:27 +0200

    added httpClient

---
+++ src/main/java/com/splot/todoaapp/service/HttpClient.java
@@ -0,0 +1,30 @@
+package com.splot.todoaapp.service;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class HttpClient {
+    private final CloseableHttpClient httpClient = HttpClients.createDefault();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    {
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    public <T> T get(String url, Class<T> clazz) {
+        HttpGet request = new HttpGet(url);
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
+            return objectMapper.readValue(response.getEntity().getContent(), clazz);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't fetch info from URL: " + url, e);
+        }
+    }
+}
