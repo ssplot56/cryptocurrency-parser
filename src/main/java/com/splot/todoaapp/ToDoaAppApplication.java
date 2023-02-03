309f69aeb85d868842a0f4e18eeb2aafe7955656
ssplot56 <ssplot56@gmail.com>
2023-02-03 18:17:07 +0200

    initial commit

---
+++ src/main/java/com/splot/todoaapp/ToDoaAppApplication.java
@@ -0,0 +1,13 @@
+package com.splot.todoaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoaAppApplication.class, args);
+    }
+
+}
